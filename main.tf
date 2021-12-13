da77143f5688af651e76c2f2903d66a7a2dbc0d6
Arne Haagensen <arnehaagensen@gmail.com>
2021-12-13 10:34:52 +0100

    Update main.tf
    updated topic name

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_sns_topic" "budgetalarms" {
 
 resource "aws_sns_topic_policy" "allow_budgets" {
   provider = aws.us-east-1
-  arn    = aws_sns_topic.budget.arn
+  arn    = aws_sns_topic.budgetalarms.arn
   policy = data.aws_iam_policy_document.sns.json
 }
 
