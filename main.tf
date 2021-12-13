1f1222b0de89aab8b3613b1f97fe0eb92bbd330b
Arne Haagensen <arnehaagensen@gmail.com>
2021-12-13 12:48:54 +0100

    Update main.tf
    empty config

--- main.tf
+++ main.tf
@@ -10,46 +10,4 @@ locals {
 }
 
 
-###################################################
-#                                                 #
-# Account Budget                                  #
-#                                                 #
-###################################################
-resource "aws_budgets_budget" "this" {
-  provider          = aws.us-east-1
-  name              = "${var.tags["environment"]}-${var.name_prefix}-monthly-budget"
-  budget_type       = "COST"
-  limit_amount      = var.account_budget_limit_in_usd
-  limit_unit        = "USD"
-  time_period_end   = "2087-06-15_00:00"
-  time_period_start = "2017-07-01_00:00"
-  time_unit         = "MONTHLY"
-  notification {
-    comparison_operator        = "GREATER_THAN"
-    threshold                  = 100
-    threshold_type             = "PERCENTAGE"
-    notification_type          = "FORECASTED"
-    subscriber_sns_topic_arns  = [aws_sns_topic.budgetalarms.arn]
-    subscriber_email_addresses = var.notification_emails
-  }
-}
 
-resource "aws_sns_topic" "budgetalarms" {
-  provider = aws.us-east-1
-  name = "${var.tags["environment"]}-${var.name_prefix}-monthly-budget"
-  tags = var.tags
-}
-
-resource "aws_sns_topic_policy" "budgetalarms_budgets" {
-  provider = aws.us-east-1
-  arn    = aws_sns_topic.budgetalarms.arn
-  policy = data.aws_iam_policy_document.sns.json
-}
-
-resource "aws_sns_topic_subscription" "budgetalarms_to_pagerduty" {
-  provider               = aws.us-east-1
-  endpoint               = "https://events.pagerduty.com/integration/7b03ab3499434e0fc08abdf0b81f68e1/enqueue"
-  protocol               = "https"
-  endpoint_auto_confirms = true
-  topic_arn              = aws_sns_topic.budgetalarms.arn
-}
