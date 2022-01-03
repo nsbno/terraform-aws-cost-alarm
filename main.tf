62d6867aa5593880081987735c9d2d3e271aa5ef
Arne Haagensen <arnehaagensen@gmail.com>
2022-01-03 15:51:58 +0100

    Update main.tf
    removed us topic

--- main.tf
+++ main.tf
@@ -33,12 +33,6 @@ resource "aws_budgets_budget" "this" {
   }
 }
 
-resource "aws_sns_topic" "budget_us" {
-  provider = aws.us-east-1
-  name = "${var.tags["environment"]}-${var.name_prefix}-monthly-budget-us"
-  tags = var.tags
-}
-
 resource "aws_sns_topic" "budget" {
   name = "${var.tags["environment"]}-${var.name_prefix}-monthly-budget"
   tags = var.tags
