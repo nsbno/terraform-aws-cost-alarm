3ff2608fe1ddfcf953a245b5aca9b26c9348c886
Arne Haagensen <arnehaagensen@gmail.com>
2023-06-22 10:38:35 +0200

    Update simple.tf

--- examples/simple.tf
+++ examples/simple.tf
@@ -1,7 +1,7 @@
 data "aws_default_tags" "tags" {}
 
 module "budget" {
-  source                      = "github.com/nsbno/terraform-aws-cost-alarm?ref=3a8a43e"  
+  source                      = "github.com/nsbno/terraform-aws-cost-alarm?ref=0.1.1"  
   name_prefix                 = var.name_prefix
   notification_emails         = ["teamemail@vy.no"]
   account_budget_limit_in_usd = var.account_budget_limit_in_usd
